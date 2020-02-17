d133c60acbccce1274676a1e35b331152a085e80
Rick Korkmaz <k.korkmaz@student.han.nl>
2020-02-17 13:18:38 +0100

    GE01: Generates CSS2-compliant result

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -4,7 +4,49 @@ import nl.han.ica.icss.ast.*;
 
 public class Generator {
 
+	private final String NEWLINE = "\n";
+	private final String TAB = "\t";
+
 	public String generate(AST ast) {
-        return "";
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (ASTNode node : ast.root.getChildren()) {
+			stringBuilder.append(walkTree(node));
+		}
+
+		return stringBuilder.toString();
+	}
+
+	private String walkTree(ASTNode node) {
+		StringBuilder stringBuilder = new StringBuilder();
+
+		if (node instanceof Stylerule) {
+			Stylerule styleruleNode = (Stylerule) node;
+
+			for (Selector selector : styleruleNode.selectors) {
+				stringBuilder
+						.append(selector.toString())
+						.append(" ");
+			}
+
+			stringBuilder.append("{" + NEWLINE);
+
+			for (ASTNode declaration : node.getChildren()) {
+				if (declaration instanceof Declaration) {
+					Declaration declarationNode = (Declaration) declaration;
+					stringBuilder
+							.append(TAB)
+							.append(declarationNode.property.name)
+							.append(": ")
+							.append(declarationNode.expression.toString());
+					stringBuilder
+							.append(";")
+							.append(NEWLINE);
+				}
+			}
+			stringBuilder.append("}" + NEWLINE);
+		}
+
+		return stringBuilder.toString();
 	}
 }
